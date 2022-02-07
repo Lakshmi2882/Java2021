fbd711ef00358f3caea7d5e51b8d345179c03e15
Radhika eddanapudi <sbharadwaj06@gmail.com>
2022-02-07 12:16:10 +0000

    new

---
+++ src/stackoverflowErrorp.java
@@ -0,0 +1,15 @@
+
+public class stackoverflowErrorp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		while(i!=0)
+		{i++;
+		System.out.println(i);
+		}
+	}
+
+}
+//it is runtime error, when you are running infinite loop, when the common memory allocation is full
+//you can not catch it by using try catch block
